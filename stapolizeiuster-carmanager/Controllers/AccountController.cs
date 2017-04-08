ab6570a4a4ffa90ddbea25fd55e4f639a37fd6da
Luca <luca.marti@bluewin.ch>
2017-04-08 13:03:29 +0200

    merge

---
+++ stapolizeiuster-carmanager/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.IdentityModel.Services;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace stapolizeiuster_carmanager.Controllers
+{
+    public class AccountController : Controller
+    {
+        
+    }
+}
--- stapolizeiuster-carmanager/Controllers/HomeController.cs
+++ stapolizeiuster-carmanager/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using stapolizeiuster_carmanager.Models;
+using System.Security.Claims;
+using System.IdentityModel.Services;
 
 namespace stapolizeiuster_carmanager.Controllers
 {
@@ -13,8 +15,36 @@ namespace stapolizeiuster_carmanager.Controllers
 
         public ActionResult Index()
         {
+            var nameClaim = ClaimsPrincipal.Current.FindFirst("name");
+
+            if (nameClaim != null && !string.IsNullOrEmpty(nameClaim.Value))
+                ViewBag.Name = nameClaim.Value;
+
             var datetime = DateTime.Now;
             return View(db.Plannings.Where(x => x.StartTime <= datetime && x.EndTime >= datetime).ToList());
+        }
+
+        public ActionResult Login(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !this.Url.IsLocalUrl(returnUrl))
+            {
+                returnUrl = "/";
+            }
+
+            // you can use this for the 'authParams.state' parameter
+            // in Lock, to provide a return URL after the authentication flow.
+            ViewBag.State = "ru=" + HttpUtility.UrlEncode(returnUrl);
+
+            return this.View();
+        }
+
+        public ActionResult Logout()
+        {
+            // Clear the session cookie
+            FederatedAuthentication.SessionAuthenticationModule.SignOut();
+
+            // Redirect to Auth0's logout endpoint
+            return RedirectToAction("Index");
         }
     }
 }
